10210d7bdb9b0d8aebf17de214157a4264488a41
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-09 08:58:11 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,10 +24,8 @@ def total_even_pairs(src)
     while row_index < src.count do
       element_index = 0
         if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
-          total = total + src[row_index][0] #&& src[row_index][1]
+          total = total + src[row_index][0] 
           total = total + src[row_index][1]
-        #elsif src[row_index][1] % 2 == 0 
-         # total = total + src[row_index][1]
         end 
       element_index += 1
       row_index += 1
